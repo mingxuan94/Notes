4072cd84977c4519551cd9c16eda37e2006d1c11
nauxgnim <leemingxuan94@gmail.com>
2020-05-24 19:08:15 +0800

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Ming Xuan on 24/5/20.
+//  Copyright © 2020 MX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
